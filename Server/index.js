9a0ed3806a695a7b1db424dd593a2b23a3f6d3e8
GauravASY <gauravyadav199808@gmail.com>
2024-12-05 22:19:48 +0530

    bugs fixed

--- Server/index.js
+++ Server/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin:  "https://order-up-c8pt.vercel.app",
+    origin:  "https://order-up-c8pt.vercel.app/",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,   
   }));
